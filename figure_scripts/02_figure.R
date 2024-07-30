b5596506eccdc0f9eb89f62e2f6ddf10650dbfc6
Kai Aragaki <adamaragaki@gmail.com>
2024-07-30 15:35:05 -0400

    rough finish of figure 2

--- figure_scripts/02_figure.R
+++ figure_scripts/02_figure.R
@@ -55,3 +55,86 @@ lp <- filter(both, `mRNA cluster` == "Luminal_papillary")
 not_lp <- filter(both, `mRNA cluster` != "Luminal_papillary")
 
 tapply(not_lp, ~ `mRNA cluster`, \(x) t.test(lp$src, x$src))
+
+get_rppa <- function() {
+  manifest <- files() |>
+    GenomicDataCommons::filter(cases.project.project_id == "TCGA-BLCA") |>
+    GenomicDataCommons::filter(type == "protein_expression") |>
+    GenomicDataCommons::filter(access == "open") |>
+    manifest() |>
+    # Creates a 'path' feature that DESeq uses for its sampleTable argument
+    mutate(path = paste(id, file_name, sep = "/"))
+
+  barcodes <- manifest$id |>
+    UUIDtoBarcode(from_type = "file_id") |>
+    dplyr::rename(submitter_id = associated_entities.entity_submitter_id)
+
+  uuids <- manifest$id |>
+    UUIDtoUUID(to_type = "case_id")
+
+  joined_ids <- full_join(barcodes, uuids, by = "file_id")
+
+  manifest <- manifest |>
+    full_join(joined_ids, by = c("id" = "file_id")) |>
+    dplyr::rename(submitter_id = submitter_id.y) |>
+    mutate(short_id = str_sub(submitter_id, 1, 12)) |>
+    dplyr::filter(str_detect(submitter_id, "^TCGA-[[:alnum:]]{2}-[[:alnum:]]{4}-0")) |>
+    arrange(submitter_id) |>
+    distinct(short_id, .keep_all = TRUE)
+
+  get_data <- function(manifest) {
+    manifest <- manifest |>
+      dplyr::select(id, path, submitter_id, short_id, cases.case_id)
+    write_tsv(manifest, file = paste0(tempdir(), "/temp.txt"))
+    system2(
+      "gdc-client",
+      c("download",
+        "-m", paste0(tempdir(), "/temp.txt"),
+        "-d", "./01_data/00_gdcdata/")
+    )
+    manifest
+  }
+
+  manifest <- get_data(manifest)
+
+
+  read_data <- function(man) {
+    path <- paste0("./01_data/00_gdcdata/", man["path"])
+    id <- man["short_id"]
+    cat(".\n")
+    out <- read_tsv(path, show_col_types = FALSE) |>
+      select(AGID, peptide_target, protein_expression)
+    colnames(out) <- c("AGID", "peptide", id)
+    out
+  }
+
+  gdc_set_cache("./01_data/00_gdcdata", create_without_asking = T)
+  data <- apply(man, 1, read_data)
+  Reduce(\(x, y) left_join(x, y, by = c("AGID", "peptide")), data)
+}
+
+joined <- get_rppa()
+
+both_sel <- select(both, short_id, src, subtype = `mRNA cluster`)
+
+long <- pivot_longer(joined, -(AGID:peptide))
+
+all <- left_join(long, both_sel, by = c(name = "short_id")) |>
+  filter(str_detect(peptide, "SRC"),
+         !is.na(subtype))
+
+ggplot(all, aes(subtype, value)) +
+  geom_jitter(width = 0.2, alpha = 0.5, aes(color = subtype)) +
+  stat_summary(fun.data = mean_cl_normal, geom = "errorbar", width = 0.2) +
+  facet_wrap(~peptide, scales = "free")
+
+tt_data <- all |>
+  select(peptide, name, value, subtype) |>
+  pivot_wider(values_from = value, names_from = peptide)
+
+lp <- filter(tt_data, subtype == "Luminal_papillary")
+not_lp <- filter(tt_data, subtype != "Luminal_papillary")
+
+tapply(not_lp, ~ subtype, \(x) t.test(lp$SRC, x$SRC))
+tapply(not_lp, ~ subtype, \(x) t.test(lp$SRC, x$SRC_pY416))
+tapply(not_lp, ~ subtype, \(x) t.test(lp$SRC, x$SRC_pY527))
